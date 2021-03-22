baf0fec1934add10e7a9767e1570a033355e60a6
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2021-03-22 14:29:43 +0530

    Sample included

---
+++ ChartSample/ChartSample/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChartSample
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> imageSource = new List<string>();
+
+       
+        public MainPage()
+        {
+            InitializeComponent();
+            imageSource.Add("Rice.jpg");
+            imageSource.Add("Wheat.jpg");
+            imageSource.Add("Ragi.jpg");
+            imageSource.Add("Millet.jpg");
+        }
+
+        
+        private void CategoryAxis_LabelClicked(object sender, Syncfusion.SfChart.XForms.LabelClickedEventArgs e)
+        {
+            chart.ChartAnnotations.Clear();
+
+            ViewAnnotation viewAnnotation = new ViewAnnotation()
+            {
+                VerticalAlignment = ChartAnnotationAlignment.Start,
+              
+                View = new Image()
+                {
+                    Source = imageSource[(int)e.Position],
+                    Aspect = Aspect.AspectFit,
+                    HeightRequest = 90,
+                    WidthRequest=90
+                },
+
+                X1 = e.Position,
+                Y1 = (BindingContext as ViewModel).Data[(int)e.Position].YValue1
+
+            };
+
+            chart.ChartAnnotations.Add(viewAnnotation);
+
+        }
+    }
+}
---
+++ ChartSample/ChartSample/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ChartModel
+    {
+        public string XValue { get; set; }
+
+        public double YValue1 { get; set; }
+    
+    }
+    public class ViewModel
+    {
+        public ObservableCollection<ChartModel> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<ChartModel>();
+
+            Data.Add(new ChartModel() { XValue = "Yeild 1", YValue1 = 12 });
+            Data.Add(new ChartModel() { XValue = "Yeild 2", YValue1 = 34 });
+            Data.Add(new ChartModel() { XValue = "Yeild 3", YValue1 = 23 });
+            Data.Add(new ChartModel() { XValue = "Yeild 4", YValue1 = 15 });
+
+        }
+    }
+}
